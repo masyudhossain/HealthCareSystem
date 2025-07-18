c59b3a280e1021f8e3fbc6abae8da70eb4c6369d
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-07-18 15:05:49 +0530

    add repositories

---
+++ src/main/java/com/healthcare/repository/PermissionRepository.java
@@ -0,0 +1,10 @@
+package com.healthcare.repository;
+
+import com.healthcare.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    List<Permission> findByRole(String role);
+}
