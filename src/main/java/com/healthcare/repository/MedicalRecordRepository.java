4e5a56358e27f03f181eb3a40657e1ebfb53326f
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-07-18 16:35:27 +0530

    changes is file

--- src/main/java/com/healthcare/repository/MedicalRecordRepository.java
+++ src/main/java/com/healthcare/repository/MedicalRecordRepository.java
@@ -1,4 +1,10 @@
 package com.healthcare.repository;
 
-public interface MedicalRecordRepository {
+import com.healthcare.entity.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+    List<MedicalRecord> findByPatientId(Long patientId);
 }
